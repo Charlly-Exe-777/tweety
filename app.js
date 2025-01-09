a6c226a8912fd18672d9ffc5dca678acbf220e85
Charlly-Exe-777 <abdelkarimchtoui24@gmail.com>
2025-01-09 18:14:51 +0000

    committed

---
+++ app.js
@@ -0,0 +1,11 @@
+import exprees from 'express';
+
+const app = exprees();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
